d26ce9af1510f675e8e2459bec76824416818e87
Astha1121 <asthas1192081@gmail.com>
2026-06-13 09:52:50 +0530

    Add files via upload

---
+++ HybridDataFramework/utility/Helper.java
@@ -0,0 +1,31 @@
+package utility;
+
+import java.io.File;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class Helper {
+	public static void capturedScreenShot(WebDriver driver) {
+		try {
+			TakesScreenshot ss = (TakesScreenshot) driver;
+			File source = ss.getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(source, new File("./ScreenShots/EasyCalculation_" + getCurrentDateTime() + ".png"));
+			System.out.println("Captured ScreenShot - On Failure");
+		} catch (Exception e) {
+			System.out.println("Exception" + e.getMessage());
+		}
+	}
+
+	public static String getCurrentDateTime() {
+		DateFormat customFormat = new SimpleDateFormat("MM_dd_yyyy_HH_mm_ss");
+		Date currentdate = new Date();
+		return customFormat.format(currentdate);
+	}
+}
